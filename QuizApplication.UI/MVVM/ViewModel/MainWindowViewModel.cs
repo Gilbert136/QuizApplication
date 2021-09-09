8a74c895c5945bd5ad2ae889086fec57c6009d46
gilbert <gilbertkwameadu@gmail.com>
2021-09-09 18:08:40 +0000

    Add project files.

---
+++ QuizApplication.UI/MVVM/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,68 @@
+﻿using QuizApplication.UI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApplication.UI.MVVM.ViewModel
+{
+    class MainWindowViewModel: ObservableObject
+    {
+        public InformationSectionViewModel InformationSectionVM { get; set; }
+
+        private object _informationSectionView;
+
+        public object InformationSectionView
+        {
+            get { return _informationSectionView; }
+            set
+            {
+                _informationSectionView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public ContentSectionViewModel ContentSectionVM { get; set; }
+
+        private object _contentSectionView;
+
+        public object ContentSectionView
+        {
+            get { return _contentSectionView; }
+            set
+            {
+                _contentSectionView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public DetailSectionViewModel DetailSectionVM { get; set; }
+
+        private object _detailSectionView;
+
+        public object DetailSectionView
+        {
+            get { return _detailSectionView; }
+            set
+            {
+                _detailSectionView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            InformationSectionVM = new InformationSectionViewModel();
+            InformationSectionView = InformationSectionVM;
+
+            ContentSectionVM = new ContentSectionViewModel();
+            ContentSectionView = ContentSectionVM;
+
+            DetailSectionVM = new DetailSectionViewModel();
+            DetailSectionView = DetailSectionVM;
+        }
+    }
+}
