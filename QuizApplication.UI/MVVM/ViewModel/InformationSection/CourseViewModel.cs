009f2d2fac7842b56e6243945d04a18397b952af
gilbert <gilbertkwameadu@gmail.com>
2021-09-14 18:21:28 +0000

    commit changes updates

---
+++ QuizApplication.UI/MVVM/ViewModel/InformationSection/CourseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using QuizApplication.UI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApplication.UI.MVVM.ViewModel.InformationSection
+{
+    class CourseViewModel
+    {
+       
+    }
+}
--- QuizApplication.UI/MVVM/ViewModel/InformationSectionViewModel.cs
+++ QuizApplication.UI/MVVM/ViewModel/InformationSectionViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using QuizApplication.UI.Core;
+using QuizApplication.UI.MVVM.ViewModel.InformationSection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,53 @@ using System.Threading.Tasks;
 
 namespace QuizApplication.UI.MVVM.ViewModel
 {
-    class InformationSectionViewModel
+    class InformationSectionViewModel: ObservableObject
     {
+        public RelayCommand CourseViewComand { get; set; }
+
+        public RelayCommand AchievementViewComand { get; set; }
+
+
+
+        public CourseViewModel CourseViewModel { get; set; }
+        public AchievementViewModel AchievementViewModel { get; set; }
+
+
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        public InformationSectionViewModel()
+        {
+            CourseViewModel = new CourseViewModel();
+
+            AchievementViewModel = new AchievementViewModel();
+
+
+
+            CourseViewComand = new RelayCommand(o =>
+            {
+                CurrentView = CourseViewModel;
+            });
+
+            AchievementViewComand = new RelayCommand(o =>
+            {
+                CurrentView = AchievementViewModel;
+            });
+
+
+            CurrentView = CourseViewModel;
+        }
     }
 }
